b8c16b437363a828c30e6e51f40d78732268e4b3
Zukic <arminzukic996@gmail.com>
2017-11-21 01:51:33 +0100

    .

--- main.py
+++ main.py
@@ -2,9 +2,3 @@ print("--------------")
 print("----Pygram----")
 print("--------------")
 
-#    ROSES ARE RED
-#    VIOLETS ARE BLUE
-#    FACEBOOK CHAT IN SHOCK STOOD
-#    CAUSE OUR APP IS SO SO GOOD
-
-#ATOMTEST
