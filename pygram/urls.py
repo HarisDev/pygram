c787c0c57f1bb6b3f6cd663a776cebd862d17820
harisdev <haris.muharemovic@outlook.com>
2017-12-16 11:32:57 +0100

    Index fix

--- pygram/urls.py
+++ pygram/urls.py
@@ -20,5 +20,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='templates/main/index.html'), name="home")
+    url(r'^$', 'main/index.html', name='index')
 ]
