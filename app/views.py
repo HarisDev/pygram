6867efce6211a35a2b48672bec70f5e606701280
harisdev <haris.muharemovic@outlook.com>
2017-12-16 11:09:17 +0100

    Index template fix

--- app/views.py
+++ app/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-
+    context = {}
     return render(
         request,
-        'templates/index.html'
+        'main/index.html',
+        context
     )
