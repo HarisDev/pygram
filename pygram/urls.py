5c2530778b7b74fad04cbe33fa2b0fd6519269c6
harisdev <haris.muharemovic@outlook.com>
2017-12-16 11:56:19 +0100

    Index fix

--- pygram/urls.py
+++ pygram/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app.views import home
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'events.views.home', name='home')
+    url(r'^$', home, name='home')
 ]
