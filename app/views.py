459ca7d5fce00a61f868de553b3aa484baa14f6e
harisdev <haris.muharemovic@outlook.com>
2017-12-16 11:38:39 +0100

    Index fix

--- app/views.py
+++ app/views.py
@@ -3,9 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {}
     return render(
         request,
-        'main/index.html',
-        context
+        'main/index.html'
     )
--- pygram/urls.py
+++ pygram/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'main/index.html', name='index')
+    url(r'^$', 'homepage.views.index', name='index')
 ]
