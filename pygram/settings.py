a40a4db1f29f37f6df10a5b67e9cceb998d1938c
harisdev <haris.muharemovic@outlook.com>
2018-01-02 15:40:16 +0100

    Progress

--- pygram/settings.py
+++ pygram/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'pygram',
-    'reset_migrations'
+    'pygram'
 ]
 
 MIDDLEWARE = [
